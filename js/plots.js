164939523427a49a9379fc5a7cac12a6b5cac26a
Dan Hedden <dirtdog1126@hotmail.com>
2020-03-08 14:52:44 -0400

    Belly Button Biodiversity Dashboard

--- js/plots.js
+++ js/plots.js
@@ -1,7 +1,8 @@
 function init() {
+  alert('HI there')
   var selector = d3.select("#selDataset");
 
-  d3.json("samples.json").then((data) => {
+  d3.json("js/samples.json").then((data) => {
       console.log(data);
       var sampleNames = data.names;
       sampleNames.forEach((sample) => {
@@ -10,18 +11,20 @@ function init() {
           .text(sample)
           .property("value", sample)
       });
-optionChanged();
+optionChanged("940");
 })}
 
+//Call Init Function ================================================================
 init();
 
+//Declare optionChanged function (called from HTML Index file)==================================
 function optionChanged(newSample) {
   buildMetadata(newSample);
   buildCharts(newSample);
 }
 
 function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
+  d3.json("js/samples.json").then((data) => {
     var metadata = data.metadata;
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
@@ -38,10 +41,10 @@ function buildMetadata(sample) {
   });
 }
 
-function buildCharts(samples) {
-  d3.json('samples.json').then((data) => {
+function buildCharts(sample) {
+  d3.json('js/samples.json').then((data) => {
       var OTU = data.samples;
-      var resultOTU = OTU.filter(sampleObj => sampleObj.id == samples);
+      var resultOTU = OTU.filter(sampleObj => sampleObj.id == sample);
       var finalOTU = resultOTU[0];
       var otu_ids = resultOTU.otu_ids;
       var OTUCount = resultOTU[1];
@@ -49,22 +52,69 @@ function buildCharts(samples) {
       var wfreq = resultOTU.wfreq;
       var barChart = d3.select('#bar');
 
-      var data = [
-        {
-          domain: { x: [resultOTU, wfreq], y: [resultOTU, wfreq] },
-          value: 2,
-              title: { text: "Belly Button Washing Frequency" },
-              type: "indicator",
-              gauge: {axis: {range: [null, 9]}},
-          mode: "gauge+number"
-        }
-      ];
-      
-      Plotly.newPlot('gauge', data);
-              
+
+//Gauge  this works ============================================================================
+// var data = [
+// {
+//   domain: { x: [0, 1], y: [0, 1] },
+//   value: 2,
+//       title: { text: "Belly Button Washing Frequency" },
+//       type: "indicator",
+//       gauge: {axis: {range: [null, 9]}},
+//   mode: "gauge+number"
+// }
+// ];
+
+// Plotly.newPlot('gauge', data);
+var traceA = {
+  type: "pie",
+  showlegend: false,
+  hole: 0.4,
+  rotation: 90,
+  values: [100 / 5, 100 / 5, 100 / 5, 100 / 5, 100 / 5, 100],
+  text: ["Very Low", "Low", "Average", "Good", "Excellent", ""],
+  direction: "clockwise",
+  textinfo: "text",
+  textposition: "inside",
+  marker: {
+    colors: ["rgba(255, 0, 0, 0.6)", "rgba(255, 165, 0, 0.6)", "rgba(255, 255, 0, 0.6)", "rgba(144, 238, 144, 0.6)", "rgba(154, 205, 50, 0.6)", "white"]
+  },
+  labels: ["0-10", "10-50", "50-200", "200-500", "500-2000", ""],
+  hoverinfo: "label"
+};
+
+var degrees = 115, radius = .6;
+var radians = degrees * Math.PI / 180;
+var x = -1 * radius * Math.cos(radians);
+var y = radius * Math.sin(radians);
+
+var layout = {
+  shapes:[{
+      type: 'line',
+      x0: 0,
+      y0: 0,
+      x1: x,
+      y1: 0.5,
+      line: {
+        color: 'black',
+        width: 8
+      }
+    }],
+  title: 'Number of Printers Sold in a Week',
+  xaxis: {visible: false, range: [-1, 1]},
+  yaxis: {visible: false, range: [-1, 1]}
+};
+
+var data = [traceA];
+
+Plotly.plot(gaugeDiv, data, layout, {staticPlot: true});
+//Top 10 Bar Graph ==========================================================================       
+
       var trace = {
-          x: [otu_ids],
-          y: [sample_values],
+          //x: otu_ids,
+          x: [40,40,47,50,51,71,78,113,126,163],
+          //y: sample_values,
+          y: ["OTU 1977", "OTU 2318", "OTU 189", "OTU 352", "OTU 1189", "OTU 41", "OTU 2264", "OTU 482", "OTU 2859", "OTU 1167"],
           type: 'bar',
           orientation: 'h'
       };
@@ -75,16 +125,18 @@ function buildCharts(samples) {
       };        
       Plotly.newPlot('bar', data, layout);
 
+//Bubble PLOT ============================================================================
       var trace1 ={
-          x: [resultOTU],
-          y: [sample_values],
+          //x:
+          x: [1167, 2859, 482, 2264, 41, 1189, 352, 189, 2318, 1977, 3450, 874, 1959, 2191, 1950, 2077],
+          y: [163, 126, 113, 78, 71, 51, 50, 47, 40, 40, 37, 36, 30, 28, 25, 23],
           mode: 'markers',
           marker: {
-              size: [sample_values],
-              color: [otu_ids],
+              size: [163, 126, 113, 78, 71, 51, 50, 47, 40, 40, 37, 36, 30, 28, 25, 23],
+              color: [1167, 2859, 482, 2264, 41, 1189, 352, 189, 2318, 1977, 3450, 874, 1959, 2191, 1950, 2077],
           }
       };
-    
+   
       var data = [trace1];
       var layout = {
           title: 'Quantity of Bacteria by OTU_ID',
@@ -93,6 +145,3 @@ function buildCharts(samples) {
       Plotly.newPlot('bubble', data, layout);      
   });
 };
-
-
-
